f46740e2e80f4fa33caa4c6b0c305b13c0afd059
replay20 <taglador@gmail.com>
2024-03-09 17:03:30 +0100

    zad 5 poprawione

--- zad5.py
+++ zad5.py
@@ -1,16 +1,14 @@
-def liczDoskonala(liczba):
-    a = 1
-    b = 2
-    while b*b < liczba:
-        if liczba % b == 0:
-            a += b + liczba/b
-        b += 1
-    if b*b == liczba:
-        a += k
-    return a == liczba
+def liczDoskonala():
+    for liczba in range(1,1000,1):
+        a = 1
+        b = 2
+        while b * b < liczba:
+            if liczba % b == 0:
+                a += b + liczba / b
+            b += 1
+        if b * b == liczba:
+            a += b
+        if a == liczba:
+            print(liczba)
 
-liczba = int(input("Podaj liczbe: "))
-if liczDoskonala(liczba):
-    print(f"Liczba {liczba} jest doskonala")
-else:
-    print(f"Liczba {liczba} nie jest doskonala")
+liczDoskonala()
